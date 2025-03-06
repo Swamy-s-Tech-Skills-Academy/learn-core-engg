fc220368989e5caa900a1dfbff353d1a88d46ab8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-06 12:47:31 +0530

    Changes

--- algos/solutions/a1twopointers/JS/palindrome/algo.js
+++ algos/solutions/a1twopointers/JS/palindrome/algo.js
@@ -1,10 +1,20 @@
 // algo.js
+function isAlphaNumeric(char) { return /[a-z0-9]/.test(char); }
+
 function isPalindrome(inputString) {
     inputString = inputString.toLowerCase();
 
     let left = 0, right = inputString.length - 1;
 
     while (left < right) {
+        while (left < right && !isAlphaNumeric(inputString[left])) {
+            left++;
+        }
+
+        while (left < right && !isAlphaNumeric(inputString[right])) {
+            right--;
+        }
+
         if (inputString[left] !== inputString[right]) {
             return false;
         }
