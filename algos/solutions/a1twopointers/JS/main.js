1c5a56b19bf0f45434e318592ea8123986e34fe7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-26 11:18:49 +0530

    Update main.js

--- algos/solutions/a1twopointers/JS/main.js
+++ algos/solutions/a1twopointers/JS/main.js
@@ -3,18 +3,26 @@ const { isPalindrome } = require("./palindrome/algo");
 const { ListNode, removeNthFromEnd } = require("./removenthfromend/algo");
 const { threeSum } = require("./threesum/algo");
 
-// Import helper test functions from separate files
+// Import helper functions from separate files
 const { showValidPalindromeDemo } = require("./palindrome/helper_palindrome");
 const { showRemoveNthNodeDemo } = require("./removenthfromend/helper_removenth");
 const { showThreeSumDemo } = require("./threesum/helper_threesum");
 
-
 // -------------------- Main Execution --------------------
 function main() {
+    console.log("=".repeat(50));
+    console.log("✅ Palindrome Algorithm Demonstration");
+    console.log("=".repeat(50));
     showValidPalindromeDemo(isPalindrome);
 
+    console.log("\n" + "=".repeat(50));
+    console.log("✅ Remove Nth Node Algorithm Demonstration");
+    console.log("=".repeat(50));
     showRemoveNthNodeDemo(removeNthFromEnd, ListNode);
 
+    console.log("\n" + "=".repeat(50));
+    console.log("✅ Three Sum Algorithm Demonstration");
+    console.log("=".repeat(50));
     showThreeSumDemo(threeSum);
 }
 
