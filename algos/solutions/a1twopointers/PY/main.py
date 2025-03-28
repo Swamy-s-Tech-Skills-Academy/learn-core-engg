85728a556d819af2a951817459cd35e129415552
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-28 21:23:13 +0530

    Initial Version

---
+++ algos/solutions/a1twopointers/PY/main.py
@@ -0,0 +1,24 @@
+from palindrome.helper_palindrome import show_valid_palindrome_demo
+# from removenthfromend.helper_removenth import show_remove_nth_node_demo
+# from threesum.helper_threesum import show_three_sum_demo
+
+
+def main() -> None:
+    print("=" * 50)
+    print("✅ Palindrome Algorithm Demonstration")
+    print("=" * 50)
+    show_valid_palindrome_demo()
+
+    # print("\n" + "=" * 50)
+    # print("✅ Remove Nth Node from End Algorithm Demonstration")
+    # print("=" * 50)
+    # show_remove_nth_node_demo()
+
+    # print("\n" + "=" * 50)
+    # print("✅ Three Sum Algorithm Demonstration")
+    # print("=" * 50)
+    # show_three_sum_demo()
+
+
+if __name__ == "__main__":
+    main()
