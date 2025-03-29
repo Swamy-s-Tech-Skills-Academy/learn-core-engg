983173ebacc1ee448b0e5c939ba881d01a55ee2a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-29 20:01:41 +0530

    Swamy/29mar work (#18)
    * Delete A.txt
    
    * Polindrome

---
+++ algos/solutions/a1twopointers/Go/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"a1twopointers/palindrome"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Repeat("=", 50))
+	fmt.Println("✅ Palindrome Algorithm Demonstration")
+	fmt.Println(strings.Repeat("=", 50))
+	palindrome.ShowValidPalindromeDemo()
+
+	// fmt.Println("\n" + strings.Repeat("=", 50))
+	// fmt.Println("✅ Remove Nth Node Algorithm Demonstration")
+	// fmt.Println(strings.Repeat("=", 50))
+	// removenthfromend.ShowRemoveNthNodeDemo()
+
+	// fmt.Println("\n" + strings.Repeat("=", 50))
+	// fmt.Println("✅ Three Sum Algorithm Demonstration")
+	// fmt.Println(strings.Repeat("=", 50))
+	// threesum.ShowThreeSumDemo()
+}
