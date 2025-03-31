3eebacce60c9948be77df054d6fec07c72e9a197
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-31 10:35:55 +0530

    Palindrome

--- algos/solutions/a1twopointers/CS/a1twopointers/PalindromeDemo/Palindrome.cs
+++ algos/solutions/a1twopointers/CS/a1twopointers/PalindromeDemo/Palindrome.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace a1twopointers.PalindromeDemo;
 
-namespace a1twopointers.PalindromeDemo
+internal sealed class Palindrome
 {
-    internal class Palindrome
+    public static bool IsValidPalindrome(string s)
     {
+        int left = 0, right = s.Length - 1;
+        while (left < right)
+        {
+            while (left < right && !char.IsLetterOrDigit(s[left]))
+            {
+                left++;
+            }
+
+            while (left < right && !char.IsLetterOrDigit(s[right]))
+            {
+                right--;
+            }
+
+            if (char.ToLower(s[left]) != char.ToLower(s[right]))
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
--- algos/solutions/a1twopointers/CS/a1twopointers/PalindromeDemo/PalindromeHelper.cs
+++ algos/solutions/a1twopointers/CS/a1twopointers/PalindromeDemo/PalindromeHelper.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace a1twopointers.PalindromeDemo;
 
-namespace a1twopointers.PalindromeDemo
+internal sealed class PalindromeHelper
 {
-    internal class PalindromeHelper
+    public static void DemoPalindrome()
     {
+        string[] testCases =
+        {
+            "RACEACAR",               // Mixed-case; should be a palindrome.
+            "A",                      // Single character: always a palindrome.
+            "ABCDEFGFEDCBA",          // Perfect palindrome.
+            "ABC",                    // Not a palindrome.
+            "ABCBA",                  // Palindrome (odd-length).
+            "ABBA",                   // Palindrome (even-length).
+            "No lemon, no melon",     // Palindrome with spaces.
+            "Was it a car or a cat I saw?", // Palindrome with punctuation.
+            "Able was I, I saw Elba", // Classic palindrome phrase.
+            "12321",                  // Numeric palindrome.
+            "123421",                 // Not a palindrome.
+            "",                       // Empty string: considered a palindrome.
+            "   "                     // Spaces only; when sanitized, becomes empty → palindrome.
+        };
+
+        foreach (string testCase in testCases)
+        {
+            bool result = Palindrome.IsValidPalindrome(testCase);
+            WriteLine($"\"{testCase}\" is palindrome? {result}");
+        }
     }
 }
