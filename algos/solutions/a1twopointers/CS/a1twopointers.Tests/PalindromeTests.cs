df818abc3697cff80434bd65b8f9e021923b08fc
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-31 10:51:15 +0530

    Validated

---
+++ algos/solutions/a1twopointers/CS/a1twopointers.Tests/PalindromeTests.cs
@@ -0,0 +1,30 @@
+﻿using a1twopointers.PalindromeDemo;
+using Xunit;
+
+namespace a1twopointers.Tests;
+
+public class PalindromeTests
+{
+    [Theory]
+    [InlineData("RACEACAR", false)]
+    [InlineData("A", true)]
+    [InlineData("ABCDEFGFEDCBA", true)]
+    [InlineData("ABC", false)]
+    [InlineData("ABCBA", true)]
+    [InlineData("ABBA", true)]
+    [InlineData("No lemon, no melon", true)]
+    [InlineData("Was it a car or a cat I saw?", true)]
+    [InlineData("Able was I, I saw Elba", true)]
+    [InlineData("12321", true)]
+    [InlineData("123421", false)]
+    [InlineData("", true)]
+    [InlineData("   ", true)]
+    public void IsValidPalindrome_ShouldReturnExpectedResult(string input, bool expected)
+    {
+        // Act
+        bool result = Palindrome.IsValidPalindrome(input);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
