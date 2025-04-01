cf13e3d0e9f23bc15d2c8c61fc442f148011fd10
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-01 20:59:30 +0530

    Changes

---
+++ algos/solutions/a1twopointers/CS/A1TwoPointersDemo/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("a1twopointers.Tests")]
---
+++ algos/solutions/a1twopointers/CS/A1TwoPointersDemo/PalindromeAlgo/Palindrome.cs
@@ -0,0 +1,31 @@
+﻿namespace A1TwoPointersDemo.PalindromeAlgo;
+
+internal sealed class Palindrome
+{
+    public static bool IsValidPalindrome(string s)
+    {
+        int left = 0, right = s.Length - 1;
+        while (left < right)
+        {
+            while (left < right && !char.IsLetterOrDigit(s[left]))
+            {
+                left++;
+            }
+
+            while (left < right && !char.IsLetterOrDigit(s[right]))
+            {
+                right--;
+            }
+
+            if (char.ToLower(s[left]) != char.ToLower(s[right]))
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
