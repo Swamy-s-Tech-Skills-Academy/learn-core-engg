fa5aa5062f77f33724fd45c253f21b78dd395995
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-04 12:57:27 +0530

    Changes

--- algos/solutions/a2fastslowpointers/PY/main.py
+++ algos/solutions/a2fastslowpointers/PY/main.py
@@ -3,11 +3,11 @@ from circulararray.helper_circulararray import show_circular_array_loop_demo
 
 
 def main() -> None:
-    print("=" * 50)
-    print("✅ Happy Number Algorithm Demonstration (Fast & Slow Pointers)")
-    print("=" * 50)
-    show_happy_number_demo()
-    print("=" * 50)
+    # print("=" * 50)
+    # print("✅ Happy Number Algorithm Demonstration (Fast & Slow Pointers)")
+    # print("=" * 50)
+    # show_happy_number_demo()
+    # print("=" * 50)
 
     print("=" * 50)
     print("✅ Circular Array Loop Algorithm Demonstration (Fast & Slow Pointers)")
