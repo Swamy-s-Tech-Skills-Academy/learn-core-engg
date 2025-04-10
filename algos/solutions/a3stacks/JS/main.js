1fec08ca191841cfa4ab5193d817604c3edc5adb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-10 12:46:58 +0530

    Initial Version

--- algos/solutions/a3stacks/JS/main.js
+++ algos/solutions/a3stacks/JS/main.js
@@ -4,12 +4,18 @@ const { calculator } = require("./calculator/algo");
 
 // Helpers
 const { showCalculatorDemo } = require("./calculator/helper_calculator");
+const { showHashMapDemo } = require("./hashmap/helper_hashmap");
 
 function main() {
     console.log("\n" + "=".repeat(50));
     console.log("✅ Calculator Algorithm Demonstration");
     console.log("=".repeat(50));
     showCalculatorDemo(calculator);
+
+    console.log("\n" + "=".repeat(50));
+    console.log("✅ HashMap Algorithm Demonstration");
+    console.log("=".repeat(50));
+    showHashMapDemo();
 }
 
 main();
