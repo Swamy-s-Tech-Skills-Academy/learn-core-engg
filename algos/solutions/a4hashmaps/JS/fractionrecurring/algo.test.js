b9eca5685644f7a9091cdbcbbba3d285f652c404
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-11 12:39:30 +0530

    Update algo.test.js

--- algos/solutions/a4hashmaps/JS/fractionrecurring/algo.test.js
+++ algos/solutions/a4hashmaps/JS/fractionrecurring/algo.test.js
@@ -18,8 +18,8 @@ test("2/3 should be '0.(6)'", () => {
     expect(fractionToDecimal(2, 3)).toBe("0.(6)");
 });
 
-test("47/18 should be '2.(611)'", () => {
-    expect(fractionToDecimal(47, 18)).toBe("2.(611)");
+test("47/18 should be '2.6(1)'", () => {
+    expect(fractionToDecimal(47, 18)).toBe("2.6(1)");
 });
 
 test("93/7 should be '13.(285714)'", () => {
@@ -30,8 +30,8 @@ test("-5/333 should be '-0.(015)'", () => {
     expect(fractionToDecimal(-5, 333)).toBe("-0.(015)");
 });
 
-test("47/-18 should be '-2.(611)'", () => {
-    expect(fractionToDecimal(47, -18)).toBe("-2.(611)");
+test("47/-18 should be '-2.6(1)'", () => {
+    expect(fractionToDecimal(47, -18)).toBe("-2.6(1)");
 });
 
 test("-4/-2 should be '2'", () => {
