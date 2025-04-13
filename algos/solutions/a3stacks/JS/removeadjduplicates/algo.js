59677e9818bc9999d6b7854d42d28228b4a181cb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-13 13:53:57 +0530

    Update algo.js

--- algos/solutions/a3stacks/JS/removeadjduplicates/algo.js
+++ algos/solutions/a3stacks/JS/removeadjduplicates/algo.js
@@ -1,25 +1,29 @@
 // removeadjduplicates/algo.js
 
 function removeDuplicates(s) {
-    let changed = true;
+    let stack = [];
 
-    while (changed) {
-        changed = false;
-        let stack = [];
-
-        for (let i = 0; i < s.length; i++) {
-            if (stack.length > 0 && stack[stack.length - 1] === s[i]) {
-                stack.pop();
-                changed = true;
-            } else {
-                stack.push(s[i]);
-            }
+    for (let c of s) {
+        if (stack.length > 0 && stack[stack.length - 1] === c) {
+            stack.pop();
+        } else {
+            stack.push(c);
         }
-
-        s = stack.join("");
     }
 
-    return s;
+    // Convert stack to string
+    let result = stack.join("");
+
+    // Check if further passes are needed
+    let i = 0;
+    while (i < result.length - 1) {
+        if (result[i] === result[i + 1]) {
+            return removeDuplicates(result.slice(0, i) + result.slice(i + 2));
+        }
+        i++;
+    }
+
+    return result;
 }
 
 module.exports = removeDuplicates;
