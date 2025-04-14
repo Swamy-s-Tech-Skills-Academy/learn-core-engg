e5223b1a15c4beba8d073146829a7fa6d0a639d1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-14 16:25:36 +0530

    Update algo.test.js

--- algos/solutions/a3stacks/JS/calculator/algo.test.js
+++ algos/solutions/a3stacks/JS/calculator/algo.test.js
@@ -2,22 +2,24 @@
 
 const { calculator } = require('./algo');
 
-test('4 + (52 - 12) + 99 = 143', () => {
-    expect(calculator("4 + (52 - 12) + 99")).toBe(143);
-});
+describe('Calculator Tests', () => {
+    test("calculator('4 + (52 - 12) + 99') should return 143", () => {
+        expect(calculator('4 + (52 - 12) + 99')).toBe(143);
+    });
 
-test('(31 + 7) - (5 - 2) = 35', () => {
-    expect(calculator("(31 + 7) - (5 - 2)")).toBe(35);
-});
+    test("calculator('(31 + 7) - (5 - 2)') should return 35", () => {
+        expect(calculator('(31 + 7) - (5 - 2)')).toBe(35);
+    });
 
-test('(12 - 9 + 4) + ( 7 - 5) = 9', () => {
-    expect(calculator("(12 - 9 + 4) + ( 7 - 5)")).toBe(9);
-});
+    test("calculator('(12 - 9 + 4) + (7 - 5)') should return 9", () => {
+        expect(calculator('(12 - 9 + 4) + (7 - 5)')).toBe(9);
+    });
 
-test('8 - 5 + (19 - 11) + 6 + (10 + 3) = 30', () => {
-    expect(calculator("8 - 5 + (19 - 11) + 6 + (10 + 3)")).toBe(30);
-});
+    test("calculator('8 - 5 + (19 - 11) + 6 + (10 + 3)') should return 30", () => {
+        expect(calculator('8 - 5 + (19 - 11) + 6 + (10 + 3)')).toBe(30);
+    });
 
-test('56 - 44 - (27 - 17 - 1) + 7 = 10', () => {
-    expect(calculator("56 - 44 - (27 - 17 - 1) + 7")).toBe(10);
+    test("calculator('56 - 44 - (27 - 17 - 1) + 7') should return 10", () => {
+        expect(calculator('56 - 44 - (27 - 17 - 1) + 7')).toBe(10);
+    });
 });
