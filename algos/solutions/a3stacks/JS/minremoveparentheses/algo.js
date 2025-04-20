03c1ef6a0b7c7daaea4d81dfb52607fd94d8332b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-20 22:05:55 +0530

    Update algo.js

--- algos/solutions/a3stacks/JS/minremoveparentheses/algo.js
+++ algos/solutions/a3stacks/JS/minremoveparentheses/algo.js
@@ -1,27 +1,26 @@
 function minRemoveParentheses(s) {
+  // First pass: Remove invalid closing parentheses and mark potential invalid opening ones
   let stack = [];
   let s_list = Array.from(s);
 
   for (let i = 0; i < s.length; i++) {
-    let val = s[i];
-
-    if (val === '(') {
-      stack.push(i); // Push index of '('
-    } else if (val === ')') {
-      if (stack.length > 0 && s_list[stack[stack.length - 1]] === '(') {
-        stack.pop(); // Valid pair, pop the matching '(' index
+    if (s[i] === '(') {
+      stack.push(i); // Push index of opening parenthesis
+    } else if (s[i] === ')') {
+      if (stack.length > 0) {
+        stack.pop(); // Valid pair found, pop the matching '('
       } else {
-        s_list[i] = ''; // Unmatched ')', mark for removal
+        s_list[i] = ''; // Invalid closing parenthesis, mark for removal
       }
     }
   }
 
-  // Remove any unmatched '(' left in the stack
+  // Remove any unmatched opening parentheses
   while (stack.length > 0) {
     s_list[stack.pop()] = ''; // Mark unmatched '(' for removal
   }
 
-  return s_list.join(''); // Reconstruct the string without invalid parentheses
+  return s_list.join(''); // Join characters back together (empty strings will be ignored)
 }
 
 module.exports = minRemoveParentheses;
